8c915c596c4fb37d9d41b3383f624bce938a4b61
ressKim <sh21dlat@gmail.com>
2024-01-01 22:40:42 +0900

    nGrinder study project start

---
+++ src/main/java/com/study/ngrindertestproject/NGrinderTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.ngrindertestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NGrinderTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NGrinderTestProjectApplication.class, args);
+    }
+
+}
